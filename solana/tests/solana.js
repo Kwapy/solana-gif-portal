bc9338333519891869cb7e9ef14aa43301f31e4f
Daniel Kwapien <danikw9@gmail.com>
2022-04-22 16:42:48 +0200

    Solana program and testing

--- solana/tests/solana.js
+++ solana/tests/solana.js
@@ -1,11 +1,42 @@
 const anchor = require("@project-serum/anchor");
 
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
-  console.log("Starting test...")
-  anchor.setProvider(anchor.AnchorProvider.env());
+  console.log("🛰 Starting test...")
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Solana;
-  const tx = await program.rpc.initialize();
+
+  const baseAccount = anchor.web3.Keypair.generate()
+
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
   console.log("The transaction is: ", tx)
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("GIF Count: ", account.totalGifs.toString())
+
+  await program.rpc.addGif("submitedgiflink1.com", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey
+    },
+  });
+  console.log("✅ Gif %d added", (account.totalGifs+1))
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("🖼 Total Gifs: ", account.totalGifs.toString())
+
+  console.log("📝 Gif List: ", account.gifList);
 }
 
 const runMain = async() => {
